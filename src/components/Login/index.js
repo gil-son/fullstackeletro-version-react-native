4fc205d78abdba746a9231bd5eb9f35702df10bf
gil-son <gilszon@gmail.com>
2021-02-12 23:44:00 -0300

    Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
+// import React from 'react';
+// import {StyleSheet, Text, View, Image, TextInput, TouchableOpacity, StatusBar} from 'react-native';
 
 
- const Login = () =>{
+//  const Login = ({navigation}) =>{
     
-    return(
-        <View>
-            <Text>Login</Text>
-        </View>
-    );
-}
 
-export default Login;
 
+//     return(
+        
+//     );
+// }
+
+// export default Login;
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#3b5998',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//     // logo: {
+//     //   width: 150,
+//     //   height: 150,
+//     //   borderRadius: 10
+//     // },
+  
+//     input: {
+//       marginTop: 10,
+//       padding: 10,
+//       with: 300,
+//       backgroundColor: '#fff',
+//       fontSize: 15,
+//       borderRadius: 50
+//     },
+//     btn: {
+//       marginTop:4,
+//       width: 200,
+//       height: 50,
+//       borderRadius:20,
+//       justifyContent: 'space-around',
+//       alignItems: 'center',
+//       backgroundColor: '#24292e',
+//       color: '#fff'
+  
+//     }
+  
+//   });
