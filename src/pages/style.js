a6b0a0abf50e54353eeb581e4458c539e5f3b9c6
gil-son <gilszon@gmail.com>
2021-02-12 23:45:46 -0300

    Style

--- src/pages/style.js
+++ src/pages/style.js
@@ -11,11 +11,84 @@ const styles = StyleSheet.create({
         // justifyContent: 'center',
         // alignItems: 'center',
         flexDirection: 'column'
+    }, 
+    menu:{
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     },
     main:{
+        fontFamily:'Comic Sans MS',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        height: '500px',
+    },
+    footer:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        backgroundColor: 'blue',
+        flexDirection:'row'
+    },
+    box1:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '20%',
+        backgroundColor: 'green',
+        flexDirection:'row'
     }
+    ,
+    box2:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '20%',
+        backgroundColor: 'pink',
+        flexDirection:'row'
+    },
+
+    box3:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '20%',
+        backgroundColor: 'yellow',
+        flexDirection:'row'
+    },
+
+    box4:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '20%',
+        backgroundColor: 'purple',
+        flexDirection:'row'
+    }
+
+    ,
+    logo: {
+      width: 150,
+      height: 150,
+      borderRadius: 10
+    },
+  
+    input: {
+      marginTop: 10,
+      padding: 10,
+      with: 300,
+      backgroundColor: '#fff',
+      fontSize: 15,
+      borderRadius: 50
+    },
+    btn: {
+      marginTop:4,
+      width: 200,
+      height: 50,
+      borderRadius:20,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      backgroundColor: '#24292e',
+      color: '#fff'
+  
+    }
+
 });
 
 export default styles;
