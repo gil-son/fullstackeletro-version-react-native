600699fc6847ef413c86714cf06b86fbd2dce66b
gil-son <gilszon@gmail.com>
2021-02-12 23:45:04 -0300

    Produtos

--- src/pages/Produtos/index.js
+++ src/pages/Produtos/index.js
@@ -1,8 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, ImageBackground, StyleSheet, Button } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import Login from '../../components/Login';
 
 
+import FA from 'react-native-vector-icons/FontAwesome';
+import IO from 'react-native-vector-icons/Ionicons';
+
+const menu = <IO name="menu" size={30} color="#900" />;
+
+const home = <IO name="home" size={30} color="#900" />;
+const rocket = <FA name="rocket" size={30} color="#900" />;
+const book = <IO name="ios-book" size={30} color="#900" />;
+const person = <IO name="person" size={30} color="#900" />;
 
 // CSS
 import styles from '../style.js';
@@ -11,11 +21,9 @@ const Produtos = ({ navigation }) => {
 
     return (
         <View style={styles.container} >
-            <View> 
-                <Button title="Home" onPress={() => navigation.navigate('Home')}  />
-                <Button title="Sobre" onPress={() => navigation.navigate('Sobre')}  />
-                <Button title="Produtos" onPress={() => navigation.navigate('Produtos')}  />  
-            </View>
+            
+            <View style={styles.menu}><TouchableOpacity onPress={() => navigation.openDrawer()}>{menu}</TouchableOpacity></View>
+            
 
             <View style={styles.main}>
                 
@@ -25,6 +33,12 @@ const Produtos = ({ navigation }) => {
 
             </View>
 
+            <View style={styles.footer}>
+                    <View style={styles.box4}><TouchableOpacity onPress={() => navigation.navigate('Home')}>{home}</TouchableOpacity></View>
+                    <View style={styles.box1}><TouchableOpacity onPress={() => navigation.navigate('Sobre')}>{rocket}</TouchableOpacity></View>
+                    <View style={styles.box2}><TouchableOpacity onPress={() => navigation.navigate('Produtos')}>{book}</TouchableOpacity></View>
+                    <View style={styles.box3}><TouchableOpacity onPress={() => alert('.')}>{person}</TouchableOpacity></View>
+            </View>
 
         </View>
     );
