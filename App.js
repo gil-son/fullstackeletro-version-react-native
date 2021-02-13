5a64519d605d699ac308b9ec8dd6b2ff51d32826
gil-son <gilszon@gmail.com>
2021-02-12 23:40:36 -0300

    App

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 
 
 import Home from './src/pages/Home';
 import Sobre from './src/pages/Sobre';
 import Produtos from './src/pages/Produtos';
+import Login from './src/components/Login';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 
 const Stack = createStackNavigator();
 
-
+const Drawer = createDrawerNavigator();
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} 
+      <Drawer.Navigator initialRouteName="Sobre">
+          <Drawer.Screen name="Home" component={Home} 
                        options={{ title: "Bem vindo(a) a Full Stack Eletro!!",
                         headerStyle:{
                           backgroundColor: '#000'
@@ -33,7 +35,7 @@ export default function App() {
 
 
 
-          <Stack.Screen name="Sobre" component={Sobre} 
+          <Drawer.Screen name="Sobre" component={Sobre} 
                          options={{ 
                          headerStyle:{
                            backgroundColor: '#000'
@@ -45,7 +47,7 @@ export default function App() {
 
 
 
-          <Stack.Screen name="Produtos" component={Produtos} 
+          <Drawer.Screen name="Produtos" component={Produtos} 
                          options={{ title: "Confira os nossos Produtos!!",
                          headerStyle:{
                            backgroundColor: '#000'
@@ -53,17 +55,24 @@ export default function App() {
                          headerTintColor:'#fff'
                         }}
           />
-      </Stack.Navigator>  
+
+
+       
+         
+      </Drawer.Navigator>  
     </NavigationContainer>
   
   );
 }
 
 // const styles = StyleSheet.create({
-//   container: {
+//   global: {
+//     fontFamily:'Comic Sans MS',
 //     flex: 1,
 //     backgroundColor: '#fff',
 //     alignItems: 'center',
 //     justifyContent: 'center',
 //   },
 // });
+
+
