a178144f45ed1741ad3e80a38e8ece5b02c919e1
gil-son <gilszon@gmail.com>
2021-02-16 03:21:38 -0300

    Info

--- App.js
+++ App.js
@@ -4,15 +4,16 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 
 
-import Home from './src/pages/Home';
-import Sobre from './src/pages/Sobre';
-import Produtos from './src/pages/Produtos';
-import Login from './src/components/Login';
+import Home from './src/screen/Home';
+import Sobre from './src/screen/Sobre';
+import Produtos from './src/screen/Produtos';
 
-import {NavigationContainer} from '@react-navigation/native';
 
-import {createStackNavigator} from '@react-navigation/stack';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 
 const Stack = createStackNavigator();
@@ -23,56 +24,48 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Sobre">
-          <Drawer.Screen name="Home" component={Home} 
-                       options={{ title: "Bem vindo(a) a Full Stack Eletro!!",
-                        headerStyle:{
-                          backgroundColor: '#000'
-                        },
-                        headerTintColor:'#fff'
-                       }}
-          />
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home}
+          options={{
+            title: "Bem vindo(a) a Full Stack Eletro!!",
+            headerStyle: {
+              backgroundColor: '#000'
+            },
+            headerTintColor: '#fff'
+          }}
+        />
 
 
 
-          <Drawer.Screen name="Sobre" component={Sobre} 
-                         options={{ 
-                         headerStyle:{
-                           backgroundColor: '#000'
-                         },
-                         headerTintColor:'#fff'
-                        }}
-          />
-                        
+        <Drawer.Screen name="Sobre" component={Sobre}
+          options={{
+            headerStyle: {
+              backgroundColor: '#000'
+            },
+            headerTintColor: '#fff'
+          }}
+        />
 
 
 
-          <Drawer.Screen name="Produtos" component={Produtos} 
-                         options={{ title: "Confira os nossos Produtos!!",
-                         headerStyle:{
-                           backgroundColor: '#000'
-                         },
-                         headerTintColor:'#fff'
-                        }}
-          />
+
+        <Drawer.Screen name="Produtos" component={Produtos}
+          options={{
+            title: "Confira os nossos Produtos!!",
+            headerStyle: {
+              backgroundColor: '#000'
+            },
+            headerTintColor: '#fff'
+          }}
+        />
+
+        
 
 
-       
-         
-      </Drawer.Navigator>  
+      </Drawer.Navigator>
     </NavigationContainer>
-  
+
   );
 }
-
-// const styles = StyleSheet.create({
-//   global: {
-//     fontFamily:'Comic Sans MS',
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 
--- src/screen/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Screen from './Screen.js';
-
-
-export const Home = ({navigation}) => <Screen navigation = {navigation} name="Home"/>
-export const Produto = ({navigation}) => <Screen navigation = {navigation} name="Produto"/>
