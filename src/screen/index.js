6f8730e65c7a3c9e18290cae28a96615ab4e0cf2
gil-son <gilszon@gmail.com>
2021-02-15 21:07:42 -0300

    Screens

---
+++ src/screen/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Screen from './Screen.js';
+
+
+export const Home = ({navigation}) => <Screen navigation = {navigation} name="Home"/>
+export const Produto = ({navigation}) => <Screen navigation = {navigation} name="Produto"/>
